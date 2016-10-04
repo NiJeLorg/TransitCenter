b95075e1be27fd90d0bfcbe43a9e6d72942085fe
JD Godchaux <jd@nijel.org>
2016-10-04 01:23:27 -0400

    remove patterns for django 1.10

--- nycbus/nycbus/urls.py
+++ nycbus/nycbus/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url
 from django.contrib import admin
 # for redirecting accounts/profile to analysis home page for the moment
 from analysis import views
