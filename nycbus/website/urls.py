e1a1ddbf224934828a951e233077c57b2a661db8
JD Godchaux <jd@nijel.org>
2016-11-17 14:19:59 -0500

    solved the django tempate and CARTO template conficts

--- nycbus/website/urls.py
+++ nycbus/website/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import include, url
 from website import views
 
+# for creating district level pdfs
+from django_pdfkit import PDFView
+
 # urls here will all be prefixed with analysis/
 
 urlpatterns = [
     url(r'^$', views.website_index, name='website_index'),
     url(r'^district/$', views.district_analysis, name='district_analysis'),
+    url(r'^district-pdf/$', PDFView.as_view(template_name='website/district.html'), name='my-pdf'),
 
 ]
 
